85777bf661515da87bafd5326659547f7893a104
gurpreeth956 <gurpreeth956@gmail.com>
2018-06-19 19:11:40 -0400

    Character Class Creation

---
+++ Character.java
@@ -0,0 +1,9 @@
+//package pkg2dsidescroll; //(Ray's Package)
+
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+
+public class Character extends Pane {
+
+    ImageView iv;
+}
