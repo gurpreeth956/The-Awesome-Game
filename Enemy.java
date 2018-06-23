d98bf6eb4755e73628a0b58b4cf68cdb4cb04cfd
gurpreeth956 <gurpreeth956@gmail.com>
2018-06-23 13:11:18 -0400

    Enemy Update

--- Enemy.java
+++ Enemy.java
@@ -1,5 +1,20 @@
-import javafx.scene.layout.Pane;
+import javafx.scene.image.ImageView;
 
-public class Enemy extends Pane {
+public class Enemy extends Character {
+    
+    int characterXpos;
+    int characterYpos;
 
+    public Enemy(ImageView iv, int posX, int posY) {
+        super(iv, posX, posY);
+    }
+    
+    public void playerPos(int playerX, int playerY) {
+        characterXpos = playerX;
+        characterYpos = playerY;
+    }
+    
+    public void followPlayer() {
+        
+    }
 }
