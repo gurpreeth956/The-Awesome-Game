e377993010fc7ec9796abc6c17ea11358b4ee9c0
gurpreeth956 <gurpreeth956@gmail.com>
2018-06-29 23:03:54 -0400

    Fixed some glitches

--- Portal.java
+++ Portal.java
@@ -2,7 +2,7 @@ import javafx.geometry.Rectangle2D;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 
-public class Portal extends Pane{
+public class Portal extends Pane {
     
     ImageView iv;
     int offsetX = 32;
@@ -12,7 +12,7 @@ public class Portal extends Pane{
     int x; //Portal xPos
     int y; //Portal yPos
     
-    public Portal(ImageView iv, int screenWidth, int screenHeight){
+    public Portal(ImageView iv, int screenWidth, int screenHeight) {
 	this.iv = iv;
 	this.iv.setViewport(new Rectangle2D(offsetX, offsetY, this.width, this.height));
 	this.x = (int)(Math.random() * screenWidth);
@@ -22,15 +22,15 @@ public class Portal extends Pane{
 	this.getChildren().addAll(iv);
     }
     
-    public boolean summon(){
-	return Math.random() < 0.01;//summons enemy from portal
+    public boolean summon() { //summons enemy from portal
+	return Math.random() < 0.01;
     }
     
-    public int getX(){
+    public int getX() {
 	return x;
     }
     
-    public int getY(){
+    public int getY() {
 	return y;
     }
 }
