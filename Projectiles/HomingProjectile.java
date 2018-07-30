8c193b77651234b172f2667924b6d86cf4a2ba3c
Raymond Huang <rhuang502@gmail.com>
2018-07-30 19:03:32 -0400

    Improved projectile movement and velocity

--- Projectiles/HomingProjectile.java
+++ Projectiles/HomingProjectile.java
@@ -1,50 +1,91 @@
 package Projectiles;
+
 import Game.Character;
 
 public class HomingProjectile extends Projectile {
-    
+
     public HomingProjectile(String img, int posX, int posY, int width, int height) {
         super(img, posX, posY, width, height);
     }
-    
+
     public void move(Character player) {
         String dist = this.distance(player);
-        
-        if (dist.equals("up")) {
+
+        if (dist.equals("U")) {
             this.setTranslateX(this.getTranslateX() + 0);
             this.setTranslateY(this.getTranslateY() - this.getVelocityY());
-            this.y-=5;
-        } 
-        if (dist.equals("down")) {
+            this.y -= 3;
+        }
+        if (dist.equals("D")) {
             this.setTranslateX(this.getTranslateX() + 0);
             this.setTranslateY(this.getTranslateY() + this.getVelocityY());
-            this.y+=5;
+            this.y += 3;
         }
-        if (dist.equals("left")) {
+        if (dist.equals("L")) {
             this.setTranslateX(this.getTranslateX() - this.getVelocityX());
             this.setTranslateY(this.getTranslateY() + 0);
-            this.x-=5;
+            this.x -= 3;
         }
-        if (dist.equals("right")) {
+        if (dist.equals("R")) {
             this.setTranslateX(this.getTranslateX() + this.getVelocityX());
             this.setTranslateY(this.getTranslateY() + 0);
-            this.x+=5;
+            this.x += 3;
+        }
+        if (dist.equals("NE")) {
+            this.setTranslateX(this.getTranslateX() + this.getVelocityX());
+            this.setTranslateY(this.getTranslateY() - this.getVelocityY());
+            this.x += 3;
+            this.y -= 3;
+        }
+        if (dist.equals("NW")) {
+            this.setTranslateX(this.getTranslateX() - this.getVelocityX());
+            this.setTranslateY(this.getTranslateY() - this.getVelocityY());
+            this.x -= 3;
+            this.y -= 3;
+        }
+        if (dist.equals("SE")) {
+            this.setTranslateX(this.getTranslateX() + this.getVelocityX());
+            this.setTranslateY(this.getTranslateY() + this.getVelocityY());
+            this.x += 3;
+            this.y += 3;
+        }
+        if (dist.equals("SW")) {
+            this.setTranslateX(this.getTranslateX() - this.getVelocityX());
+            this.setTranslateY(this.getTranslateY() + this.getVelocityY());
+            this.x -= 3;
+            this.y += 3;
         }
     }
-    
+
     public String distance(Character player) {
         int vert = player.getY() - this.y;
         int hori = player.getX() - this.x;
-        
-        if(Math.abs(vert) > Math.abs(hori)) {
-            if(vert <= 0) {
-                return "up";
-            }
-            return "down";
+        String dir = "";
+
+        if (hori > 0 && vert < 0) {
+            dir = "NE";
         }
-        else if(hori <= 0) {
-            return "left";
+        if (hori < 0 && vert < 0) {
+            dir = "NW";
         }
-        return "right";
+        if (hori < 0 && vert > 0) {
+            dir = "SW";
+        }
+        if (hori > 0 && vert > 0) {
+            dir = "SE";
+        }
+        if (hori > 0 && vert == 0) {
+            dir = "R";
+        }
+        if (hori < 0 && vert == 0) {
+            dir = "L";
+        }
+        if (hori == 0 && vert > 0) {
+            dir = "D";
+        }
+        if (hori == 0 && vert < 0) {
+            dir = "U";
+        }
+        return dir;
     }
 }
